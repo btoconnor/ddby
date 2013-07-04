24bd313343752b8580cca72d65e6e447ca7a8b36
Brian O'Connor <brian@btoconnor.net>
2013-07-04 09:18:59 -0400

    Account for different precision levels when exchanging currencies

--- ddby/exchange/base.py
+++ ddby/exchange/base.py
@@ -21,6 +21,7 @@ class Exchange(object):
             return money
 
         rate = self.get_rate(money.currency.code, currency.code)
+        rate = self._scale_rate(rate, money.currency, currency)
 
         value = int(round(money.amount * rate, 0))
 
@@ -35,12 +36,44 @@ class Exchange(object):
             return money
 
         rate = self.get_rate(currency.code, money.currency.code)
+        rate = self._scale_rate(rate, currency, money.currency)
 
         value = int(round(money.amount / rate, 0))
 
         return Money(value, currency)
 
+    def _scale_rate(self, rate, original, desired):
+        """Scale an exchange rate between precisions
+
+        Because ddby stores Money objects at currency precision,
+        rather than some arbitrary number of fractional units,
+        exchanging between separate currencies can get tricky.
+
+        For instance, let's say we have $5.00 USD, and we want
+        to exchange that to Japanese Yen.  The current exchange
+        rate is 1 USD = 123.5 JPY.  So we have a Money object
+        Money(500, 'USD').  We run the exchange of 500 * 123.5,
+        and then make that JPY.  However, this is result:
+
+        500 * 123.5 = 61750 JPY, which is considerably higher
+        than the desired 618 JPY we were expecting.
+
+        In order to account for this, we scale the exchange rate
+        of 123.5 to be 1.235 to account for the difference in
+        precision.
+
+        This now becomes:
+        500 * 1.235 = 617.5 JPY, rounded to 618 JPY.
+        """
+        if original.precision == desired.precision:
+            # The two currencies have the same precision, 
+            # we can avoid this whole computation
+            return rate
+
+        return (rate * pow(10, (desired.precision - original.precision)))
+
     def get_rate(self, original, desired):
         """Get the rate in order to convert original
         currency to desired currency"""
         raise NotImplementedError
+
--- tests/test_exchange.py
+++ tests/test_exchange.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import unittest
 
 from ddby import Money, Currency, get_currency
@@ -28,6 +29,49 @@ class TestExchange(unittest.TestCase):
         actual = exchange.exchange_from(m, 'EUR')
 
         assert expected == actual
+
+    def test_exchanging_precision(self):
+        m = Money(500, 'USD') # $5.00 USD
+        rates = {'USD': {'JPY': 123.5}}
+
+        exchange = static.StaticExchange(rates)
+
+        expected = Money(618, 'JPY')
+
+        actual = exchange.exchange_to(m, 'JPY')
+
+        assert expected == actual, "Actual {0} != {1}".format(
+            expected, actual
+            )
+
+    def test_exchanging_precision_from_lower_to_higher_precision(self):
+        m = Money(100, 'JPY') # $5.00 USD
+        rates = {'JPY': {'USD': 0.01}}
+
+        exchange = static.StaticExchange(rates)
+
+        expected = Money(100, 'USD')
+
+        actual = exchange.exchange_to(m, 'USD')
+
+        assert expected == actual, "Actual {0} != {1}".format(
+            expected, actual
+            )
+
+    def test_exchanging_precision_from(self):
+        m = Money(500, 'USD') # $5.00 USD
+        rates = {'JPY': {'USD': 0.01}}
+
+        exchange = static.StaticExchange(rates)
+
+        expected = Money(500, 'JPY')
+
+        actual = exchange.exchange_from(m, 'JPY')
+
+        assert expected == actual, "Actual {0} != {1}".format(
+            expected, actual
+            )
+
 
 if __name__ == '__main__':
     unittest.main()
