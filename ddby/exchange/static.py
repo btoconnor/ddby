8952ea333938bc98b3b3c3c729d676e4b7c9d012
Brian O'Connor <brian@btoconnor.net>
2013-07-27 13:50:52 -0400

    Updating documentation for static exchange to be more clear about how to use it

--- ddby/exchange/static.py
+++ ddby/exchange/static.py
@@ -5,7 +5,20 @@ __all__ = ['StaticExchange']
 class StaticExchange(Exchange):
 
     def __init__(self, values):
-        "Grab rates from an array"
+        """Establish an exchange of static rates
+
+        The format of the values list is a 2-dimensional
+        array of original currency -> desired currency
+
+        An example:
+        values = {'USD': {'GBP': 1.5, 'EUR': 1.3},
+                  'GBP': {'USD': 0.7, 'EUR': 0.9},
+                  'EUR'L {'USD': 0.8, 'GBP': 1.1}
+                 }
+
+        The above example would provide exchange rates for
+        converting money between EUR, GBP, and USD.
+        """
         self.values = values
 
     def get_rate(self, original, desired):
